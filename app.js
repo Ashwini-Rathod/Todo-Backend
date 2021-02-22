9d49f5f05ea5bf468821a6bf6b26ba078abf3e03
Ashwini-Rathod <ashwini.rathod@contentstack.com>
2021-02-22 11:16:41 +0530

    cors installed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const userRoutes = require("./Routes/userRoutes");
 const taskRoutes = require("./Routes/taskRoutes");
